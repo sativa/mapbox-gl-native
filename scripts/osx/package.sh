aa3df121098cee11f16539565cda853721fe2097
Minh Nguyễn <mxn@1ec5.org>
2015-12-05 10:38:06 -0800

    [osx] Added license back to build output

--- scripts/osx/package.sh
+++ scripts/osx/package.sh
@@ -21,37 +21,55 @@ function step { >&2 echo -e "\033[1m\033[36m* $@\033[0m"; }
 function finish { >&2 echo -en "\033[0m"; }
 trap finish EXIT
 
-step "Creating build files..."
+rm -rf ${OUTPUT}
+mkdir -p "${OUTPUT}"
+
+step "Recording library version…"
+VERSION="${OUTPUT}"/version.txt
+echo -n "https://github.com/mapbox/mapbox-gl-native/commit/" > ${VERSION}
+HASH=`git log | head -1 | awk '{ print $2 }' | cut -c 1-10` && true
+echo -n "mapbox-gl-native "
+echo ${HASH}
+echo ${HASH} >> ${VERSION}
+
+step "Creating build files…"
 export MASON_PLATFORM=osx
 export BUILDTYPE=${BUILDTYPE:-Release}
 export HOST=osx
 make Xcode/osx
 
-VERSION=${TRAVIS_JOB_NUMBER:-${BITRISE_BUILD_NUMBER:-0}}
+PROJ_VERSION=${TRAVIS_JOB_NUMBER:-${BITRISE_BUILD_NUMBER:-0}}
 
-step "Building OS X framework (build ${VERSION})..."
+step "Building framework for OS X (build ${PROJ_VERSION})…"
 xcodebuild -sdk macosx${OSX_SDK_VERSION} \
     ARCHS="x86_64" \
     ONLY_ACTIVE_ARCH=NO \
     GCC_GENERATE_DEBUGGING_SYMBOLS=${GCC_GENERATE_DEBUGGING_SYMBOLS} \
-    CURRENT_PROJECT_VERSION=${VERSION} \
+    CURRENT_PROJECT_VERSION=${PROJ_VERSION} \
     -project ./build/osx-x86_64/gyp/osx.xcodeproj \
     -configuration ${BUILDTYPE} \
     -target osxsdk \
     -jobs ${JOBS}
 
-TARGET_BUILD_DIR=gyp/build/${BUILDTYPE}
-INFOPLIST_PATH=Mapbox.framework/Versions/Current/Resources/Info.plist
+step "Copying OS X framework into place…"
+cp -r \
+    gyp/build/${BUILDTYPE}/${NAME}.framework \
+    ${OUTPUT}/${NAME}.framework
 
 # Uncomment when we're ready to release an official version.
 #VERSION=$( git tag | grep ^osx | sed 's/^osx-//' | sort -r | grep -v '\-rc.' | grep -v '\-pre.' | sed -n '1p' | sed 's/^v//' )
 #if [ "$VERSION" ]; then
+#    step "Updating Info.plist (version ${VERSION})…"
+#    INFOPLIST_PATH=${NAME}.framework/Versions/Current/Resources/Info.plist
 #    plutil \
 #        -replace CFBundleShortVersionString -string ${VERSION} \
-#        $TARGET_BUILD_DIR/$INFOPLIST_PATH
+#        ${OUTPUT}/$INFOPLIST_PATH
 #    plutil \
 #        -replace CFBundleVersion -string ${VERSION} \
-#        $TARGET_BUILD_DIR/$INFOPLIST_PATH
+#        ${OUTPUT}/$INFOPLIST_PATH
 #fi
 
-echo $TARGET_BUILD_DIR/Mapbox.framework
+stat ${OUTPUT}/${NAME}.framework
+
+step "Copying library resources…"
+cp -pv LICENSE.md "${OUTPUT}"
